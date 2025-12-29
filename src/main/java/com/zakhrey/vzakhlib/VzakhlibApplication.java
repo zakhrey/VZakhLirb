ceb723ec589400cf2edef09893360592a0682ee7
zakhrey <zakhrey@gmail.com>
2025-12-29 14:03:01 +0300

    task-1. added first page

---
+++ src/main/java/com/zakhrey/vzakhlib/VzakhlibApplication.java
@@ -0,0 +1,13 @@
+package com.zakhrey.vzakhlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VzakhlibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VzakhlibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zakhrey/vzakhlib/VzakhlibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakhrey.vzakhlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VzakhlibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
